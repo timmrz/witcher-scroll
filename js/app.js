19d3c91d885fbda838e5ae2c241de96388076e29
Тимофей Мороз <tsimafei.maroz@bk.ru>
2023-02-11 18:49:04 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper', {
+    speed: 2000,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2
+    },
+    spaceBetween: 18,
+    parallax: true,
+    freeMode: true
+})
